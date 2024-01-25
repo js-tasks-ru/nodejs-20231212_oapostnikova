08fc2762b8a5cf4c377a137c204594d37ea65193
Olga Postnikova <olga.postnikova@hotmail.com>
2024-01-25 18:57:53 +0300

    03 streams: 01 limit size

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.transferred = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.transferred += this.writableLength;
 
+    if (this.transferred > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
