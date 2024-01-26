675fe85d2a2b27430146b31adbd446bac364edaf
Olga Postnikova <olga.postnikova@hotmail.com>
2024-01-27 02:41:38 +0300

    03 streams: file server get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,48 @@
-const url = require('url');
 const http = require('http');
-const path = require('path');
+const fs = require('fs');
+
+const re = new RegExp('^/[\\w\.-]+$');
+const PUBLIC_DIR = __dirname + '/files';
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+  let fileName;
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  try {
+    fileName = decodeURIComponent(req.url);
+  } catch (err) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
+
+  console.log(fileName);
+
+  if (!re.test(fileName)) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
 
   switch (req.method) {
     case 'GET':
+      const file = fs.createReadStream(PUBLIC_DIR + fileName);
+      file.pipe(res);
+      file.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Not found');
+          return;
+        }
 
+        console.error(`Error ${err.code} ${err.message}`);
+        res.statusCode = 500;
+        res.end('Internal error');
+      });
+      res.on('close', () => {
+        file.destroy();
+      });
       break;
 
     default:
