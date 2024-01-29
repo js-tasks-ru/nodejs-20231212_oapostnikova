b153aee627b80843488a313288c4649840107963
Olga Postnikova <olga.postnikova@hotmail.com>
2024-01-29 03:48:36 +0300

    03 streams: file server delete

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,17 +1,49 @@
-const url = require('url');
 const http = require('http');
-const path = require('path');
+const fs = require('fs');
+
+const re = new RegExp('^/[\\w\.-]+$');
+const PUBLIC_DIR = __dirname + '/files';
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+  let fileName;
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  try {
+    fileName = decodeURIComponent(req.url);
+  } catch (e) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
+
+  if (!re.test(fileName)) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
 
   switch (req.method) {
     case 'DELETE':
+      fs.unlink(PUBLIC_DIR + fileName, (err) => {
+        if (!err) {
+          res.statusCode = 200;
+          res.end();
+          return;
+        }
+
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Bad request');
+
+          return;
+        }
+
+        console.error(`Error ${e.code} ${e.message}`);
+
+        res.statusCode = 500;
+        res.end('Internal error');
+      });
 
       break;
 
