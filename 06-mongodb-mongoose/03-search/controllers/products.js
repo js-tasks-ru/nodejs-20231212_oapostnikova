f68574aaac32ecaa07374929af969b61be47d94a
Olga Postnikova <olga.postnikova@hotmail.com>
2024-02-05 20:05:53 +0300

    06 mongodb mongoose

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,10 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const products = await Product.find(
+      {$text: {$search: ctx.query.query}},
+  ).populate('category');
+
+  ctx.body = {products: products.map(mapProduct)};
 };
