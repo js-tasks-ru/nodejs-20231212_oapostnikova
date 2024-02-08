3bd2dd943920d5888e8ef97c25c4ffd06cca1795
Olga Postnikova <olga.postnikova@hotmail.com>
2024-02-08 15:25:17 +0300

    08 email: 02 order

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,42 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const orderedProduct = await Product.findOne({_id: ctx.request.body.product});
+
+  const newOrder = await Order.create({
+    user: ctx.user,
+    product: orderedProduct,
+    phone: ctx.request.body.phone,
+    address: ctx.request.body.address,
+  });
+
+  sendMail({
+    template: 'order-confirmation',
+    locals: {
+      id: newOrder.id,
+      product: orderedProduct,
+    },
+    to: ctx.user.email,
+    subject: 'Подтверждение заказа',
+  });
+
+  ctx.body = {
+    order: newOrder.id,
+  };
+
+  return next();
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user.id})
+      .limit(100)
+      .populate('product')
+      .populate('user');
+
+  ctx.body = {orders: orders.map(mapOrder)};
+
+  return next();
 };
--- 08-email/02-order/models/Order.js
+++ 08-email/02-order/models/Order.js
@@ -1,7 +1,33 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
+const re = new RegExp(/\+?\d{6,14}/);
+
 const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return re.test(value);
+      },
+      message: 'Неверный формат номера телефона.',
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = connection.model('Order', orderSchema);
